1b7f6bb5ccb93fb5c9b565b9c2e822a895efcf83
leticia-chijo <leticia.chijo@gmail.com>
2023-06-28 21:42:06 -0300

    Seção: Get by id e ObjectId

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { MongoClient } from "mongodb"
+import { MongoClient, ObjectId } from "mongodb"
 import dotenv from "dotenv"
 
 // Criação do app
@@ -19,28 +19,6 @@ mongoClient.connect()
 	.then(() => db = mongoClient.db())
 	.catch((err) => console.log(err.message))
 
-// Variáveis Globais
-const receitas = [
-	{
-		id: 1,
-		titulo: "Pão com Ovo",
-		ingredientes: "Ovo e pão",
-		preparo: "Frite o ovo e coloque no pão"
-	},
-	{
-		id: 2,
-		titulo: "Mingau de Whey",
-		ingredientes: "Leite, Aveia e Whey",
-		preparo: "Mistura tudo na panela fervendo",
-	},
-	{
-		id: 3,
-		titulo: "Omelete",
-		ingredientes: "Ovo",
-		preparo: "Bata os ovos e frite na frigideira"
-	}
-]
-
 // Funções (endpoints)
 app.get("/receitas", (req, res) => {
 	const promise = db.collection("receitas").find().toArray()
@@ -51,14 +29,10 @@ app.get("/receitas", (req, res) => {
 
 app.get("/receitas/:id", (req, res) => {
 	const { id } = req.params
-	const { auth } = req.headers
 
-	if (!auth) {
-		return res.status(401).send("Faça login!")
-	}
-
-	const receita = receitas.find((rec) => rec.id === Number(id))
-	res.send(receita)
+	db.collection("receitas").findOne({ _id: new ObjectId(id) })
+		.then(receita => res.send(receita))
+		.catch(err => res.status(500).send(err.message))
 })
 
 app.post("/receitas", (req, res) => {
@@ -71,7 +45,7 @@ app.post("/receitas", (req, res) => {
 	const novaReceita = { titulo, ingredientes, preparo }
 
 	const promise = db.collection("receitas").insertOne(novaReceita)
-	
+
 	promise.then(() => res.sendStatus(201))
 	promise.catch(err => res.status(500).send(err.message))
 })
