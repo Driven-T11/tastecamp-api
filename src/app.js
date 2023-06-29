6ebfd40f59bda4e2e6cf92255084acf57bf92b22
leticia-chijo <leticia.chijo@gmail.com>
2023-06-29 20:26:33 -0300

    Seção: Validações com Joi

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { MongoClient, ObjectId } from "mongodb"
 import dotenv from "dotenv"
+import Joi from "joi"
 
 // Criação do app
 const app = express()
@@ -47,13 +48,24 @@ app.get("/receitas/:id", async (req, res) => {
 app.post("/receitas", async (req, res) => {
 	const { titulo, ingredientes, preparo } = req.body
 
-	if (!titulo || !ingredientes || !preparo) return res.status(422).send("Preencha todos os campos!")
+	const schemaReceita = Joi.object({
+		titulo: Joi.string().required(),
+		ingredientes: Joi.string().required(),
+		preparo: Joi.string().required().min(5).max(200)
+	})
+
+	const validation = schemaReceita.validate(req.body, { abortEarly: false })
+
+	if (validation.error) {
+		const errors = validation.error.details.map(detail => detail.message)
+		return res.status(422).send(errors)
+	}
 
 	try {
 		const receita = await db.collection("receitas").findOne({ titulo: titulo })
 		if (receita) return res.status(409).send("Essa receita já existe!")
 
-		await db.collection("receitas").insertOne({ titulo, ingredientes, preparo })
+		await db.collection("receitas").insertOne(req.body)
 		res.sendStatus(201)
 	} catch (err) {
 		res.status(500).send(err.message)
